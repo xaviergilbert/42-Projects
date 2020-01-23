bdfbff0149b5ba45dccbdbad7af8675eca7a4d55
Xavier GILBERT <xgilbert@e1r8p7.42.fr>
2020-01-23 16:45:55 +0100

    delete folders

--- bootcamp_python/day02/ex00/ft_filter.py
+++
@@ -1,5 +0,0 @@
-def ft_filter(function_to_apply, list_of_inputs):
-   ret = []
-   for element in list_of_inputs:
-      ret.append(element) if function_to_apply(element)
-   return (ret)
--- bootcamp_python/day02/ex00/ft_map.py
+++
@@ -1,5 +0,0 @@
-def ft_map(function_to_apply, list_of_inputs):
-    ret = []
-    for element in list_of_inputs:
-        ret.append(function_to_apply(element))
-    return ret
